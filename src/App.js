9c867501b6f28a9d841a10cb5c467e73aa5922fa
Ahmed067Abdullah <ahmed@sudofy.com>
2020-07-20 19:23:05 +0500

    days dropdown added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import ReactApexChart from 'react-apexcharts';
+import Select from 'react-select';
 import StatsModal from './components/stats-modal/StatsModal';
+import TestsGraphModal from './components/tests-graph-modal/TestsGraphModal';
 import getGraphOptions from './utils/graphOptions';
 import apiURL from './config/apiURL';
 import './App.css';
+
+const dropdownOptions = [{ label: 'Last 30 days', value: '30' }, { label: 'Last 60 days', value: '60' }, { label: 'Last 90 days', value: '90' }, { label: 'All', value: '' },]
+const defaultOption = dropdownOptions[2];
 
 function App() {
   const [lastDay, setLastDay] = useState({})
   const [cumulativeData, setCumulativeData] = useState({});
   const [nonCumulativeData, setNonCumulativeData] = useState({});
   const [dates, setDates] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(defaultOption);
   const [showCumulative, setShowCumulative] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [showTestsModal, setShowTestsModal] = useState(false);
   const [loading, setLoading] = useState(true);
   const [showDataType, setShowDataType] = useState({ Active: true, Confirmed: true, Recovered: true, Deaths: true });
 
   useEffect(() => {
-    fetch(apiURL)
+    getData(defaultOption.value);
+    const data = JSON.parse(localStorage.getItem("testt"));
+    setupState(data);
+    setLastDay(data[data.length - 1]);
+    setLoading(false);
+  }, []);
+
+  const getData = days => {
+    setLoading(true);
+    fetch(`${apiURL}?days=${days}`)
       .then(response => response.json())
       .then(data => {
+        console.log(data)
         setupState(data);
         setLastDay(data[data.length - 1]);
         localStorage.setItem("testt", JSON.stringify(data));
         setLoading(false);
       })
       .catch(err => console.log(err));
-
-    // const data = JSON.parse(localStorage.getItem("testt"));
-    // setupState(data);
-    // setLastDay(data[data.length - 1]);
-    // setLoading(false);
-  }, []);
+  };
 
   const isDataSetOk = today => {
     const { Active, Recovered } = today;
@@ -101,8 +113,31 @@ function App() {
 
   const btns = ['Active', 'Confirmed', 'Recovered', 'Deaths'];
 
+  const customStyles = {
+    menu: (provided, state) => ({
+      ...provided,
+      textAlign: 'start',
+      cursor: 'pointer',
+      color: state.selectProps.menuColor,
+    })
+  }
+
+  const renderTestsGraphModal = () => {
+    const index = nonCumulativeData.Tests.findIndex(d => d) + 1;
+    return <TestsGraphModal
+      open={showTestsModal}
+      handleClose={() => setShowTestsModal(false)}
+      dates={dates.slice(index)}
+      nonCumulativeData={(nonCumulativeData.Tests || []).slice(index)}
+      cumulativeData={(cumulativeData.Tests || []).slice(index)}
+    />;
+  };
+
   return (
     <div className="App">
+      {showTestsModal
+        ? renderTestsGraphModal()
+        : null}
       <StatsModal
         open={showModal}
         handleClose={() => setShowModal(false)}
@@ -112,19 +147,42 @@ function App() {
         <button
           className="active"
           disabled={loading}
+          onClick={() => setShowTestsModal(true)}
+        >View Tests Frequency</button>
+        <button
+          className="active"
+          disabled={loading}
           onClick={() => setShowModal(true)}
         >View Stats</button>
       </div>
-      <div className="graph-container">
+      <div className="data-container">
         {loading
           ? <p className='loading'>Loading...</p>
-          : <ReactApexChart
-            height={'100%'}
-            options={getGraphOptions(dates)}
-            series={graphData}
-            type="line"
-            width={'100%'}
-          />}
+          : <div style={{ height: '100%' }}>
+            <div className="select-container">
+              <Select
+                defaultValue={defaultOption}
+                options={dropdownOptions}
+                onChange={val => {
+                  if (val !== selectedDate) {
+                    setSelectedDate(val);
+                    getData(val.value);
+                  }
+                }}
+                value={selectedDate}
+                styles={customStyles}
+              />
+            </div>
+            <div className="graph-container">
+              <ReactApexChart
+                height={'100%'}
+                options={getGraphOptions(dates)}
+                series={graphData}
+                type="line"
+                width={'100%'}
+              />
+            </div>
+          </div>}
       </div>
       <p className='updated-on-date'>
         {loading
--- src/config/apiURL.js
+++ src/config/apiURL.js
@@ -1,1 +1,3 @@
 export default 'https://covid-visualizer-pakistan.herokuapp.com/api/data';
+
+// export default 'http://localhost:5000/api/data';
